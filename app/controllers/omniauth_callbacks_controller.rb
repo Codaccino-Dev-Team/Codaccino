e9a384e05740375ddbcfd07d163024895447d249
wmuengineer <[adam.d.bell@icloud.com]>
2014-04-17 19:04:28 -0700

    Fixed bug with twitter branch

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     user = User.from_omniauth( request.env["omniauth.auth"] )
+
     if user.persisted?
       flash[:success]= "#{user.email}, you are signed in!"
       sign_in_and_redirect user
