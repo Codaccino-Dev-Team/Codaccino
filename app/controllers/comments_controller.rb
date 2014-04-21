9fcdd040809b0151f5ec339deeddd22276de78b1
Adam Hendricksen <adam@nird.us>
2014-04-21 13:26:50 -0700

    Adjusting params to accept comment attributes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,24 @@
 class CommentsController < ApplicationController
   before_filter :load_commentable
+
   def new
     @comment = @commentable.comments.new
   end
-  
+
   def create
-    @comment = @commentable.comments.new(params[:content])
-    if @comment.save
+    @comment = @commentable.comments.new comment_params
+    if @comment.save!
       redirect_to @commentable, notice: "Comment created"
     else
       render :new
     end
-    
+
   end
-  
+
   def index
     @comments = @commentable.comments
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit :content, :shops_attributes
@@ -26,6 +27,6 @@ class CommentsController < ApplicationController
     klass= [Shop].detect {|c| params["#{c.name.underscore}_id"]}
     @commentable = klass.find(params["#{klass.name.underscore}_id"])
   end
-  
-  
+
+
 end
