8881e3777c01d35dd0cb248ac2867f14214ebb21
Adam Hendricksen <adam@nird.us>
2014-04-24 21:02:23 -0700

    Adding explicit visitor role

--- app/policies/shop_policy.rb
+++ app/policies/shop_policy.rb
@@ -1,5 +1,12 @@
 ShopPolicy = Struct.new(:user, :shop) do
   self::Scope = Struct.new(:user, :scope) do
+  def visitor?
+    if user? or admin?
+      return false
+    else
+      return true
+    end
+  end
   def user?
     return false if user.nil?
     return true if user.role == "user"
