dbcd0db0357396f55520f36e29c33e96655deb16
wmuengineer <[adam.d.bell@icloud.com]>
2014-04-24 19:43:02 -0700

    added sort features in chrome schema and test file

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140417054848) do
-
 
   create_table "dynamic_ratings", force: true do |t|
     t.decimal  "wifi_up"
---
+++ test/features/sort_by_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+feature "SortBy" do
+  scenario "the test is sound" do
+    skip " this aasdfkjadkls;f"
+    visit root_path
+    #click ok drop down
+      #check to make sure the page load right
+    #cycle through options    
+  end
+end
