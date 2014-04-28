c91b0d12976042f818fca8feb2be36ee5a72cb10
wmuengineer <[adam.d.bell@icloud.com]>
2014-04-27 19:55:00 -0700

    Added three stores to the seed.rb files

--- db/schema.rb
+++ db/schema.rb
@@ -71,9 +71,9 @@ ActiveRecord::Schema.define(version: 20140418015038) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role",                   default: "user"
     t.string   "provider"
     t.string   "uid"
+    t.string   "role",                   default: "user"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
