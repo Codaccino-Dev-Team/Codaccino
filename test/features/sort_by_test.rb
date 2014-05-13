7d1de1f506951fe46144fcf472c01a6b87d27d1f
wmuengineer <[adam.d.bell@icloud.com]>
2014-05-12 22:15:55 -0700

    Modified sort_by_test.rb file to properly assert true when shops appear in the proper order. Also revised view files to have the correct text to describe the ratings

--- test/features/sort_by_test.rb
+++ test/features/sort_by_test.rb
@@ -2,41 +2,31 @@ require "test_helper"
 
 feature "As a user I want to sort shops for various criteria" do
   scenario "can sort by wifi_down" do
-   skip 'skipping all tests associated with JS until we learn how to test in class.'
     sign_in
     visit root_path
-    save_and_open_page
-    find("option[value='download']").click
-    # page.execute_script()
-    save_and_open_page
-#    find("option[value='download']").click
-#   save_and_open_page
-    puts " Should be number one #{page.body.index('Urban City Drive-Thru and Louge')}"
-    puts " Should be number two #{page.body.index('Richmond Beach Coffee Company')}"
-    page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
+    click_on 'Upload Speed'
+    puts "#{page.body.index(shops(:shop_4).name)}"
+    puts "#{page.body.index(shops(:shop_3).name)}"
+    assert_operator page.body.index(shops(:shop_1).name), :<,  page.body.index(shops(:shop_2).name)
 
   end
   scenario "can sort by wifi_up" do
-   skip 'using other test to validate pattern'
-    visit root_path
     sign_in
-    find("option[value='upload']").click
-    current_path.must_include "wifi_up"
+    visit root_path
+    click_on 'Download Speed'
+    page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
   scenario "can sort by power" do
-    skip 'using other test to validate pattern'
-    visit root_path
     sign_in
-    find("option[value='power']").click
-    current_path.must_include "power"
+    visit root_path
+    click_on 'Power Outlets'
+    page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
   scenario "can sort by noise" do
-    skip 'using other test to validate pattern'
-    visit root_path
     sign_in
-
-    find("option[value='noise']").click
-    current_path.must_include "noise"
+    visit root_path
+    click_on 'Noise'
+    page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
 
 
