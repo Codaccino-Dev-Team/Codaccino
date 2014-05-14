9ea6ea5a73d82e9c2093eceab304fb266f5dacb7
wmuengineer <[adam.d.bell@icloud.com]>
2014-05-13 17:02:42 -0700

    removed create method in the leads controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -4,15 +4,5 @@ class LeadsController < ApplicationController
     @shops = Shop.all
   end
 
-  def create
-    @lead = Lead.create params[:lead].permit(:twitter_handle)
-    if @lead.save
-      flash[:success] = "We will Tweet @ you #{@lead.twitter_handle}"
-      redirect_to shops_index_path
-    else
-      flash[:success] = "Make sure you've added a valid twitter user name with the @ sign."
-      redirect_to root_path
 
-    end
-  end
 end
