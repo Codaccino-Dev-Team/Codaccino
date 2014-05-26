cedb49c57d5a3df285d93dd59ad60d1522188c06
wmuengineer <[adam.d.bell@icloud.com]>
2014-05-25 23:45:44 -0700

    removed save_test from tests
    https://app.asana.com/0/11389918903342/12069635699406

--- test/features/sort_by_test.rb
+++ test/features/sort_by_test.rb
@@ -11,7 +11,6 @@ feature "As a user I want to sort shops for various criteria" do
     skip 'waiting for adam to fix the pundit stuff'
     visit root_path
     click_link 'Upload Speed'
-    save_test
     page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
   scenario "can sort by power" do
@@ -19,14 +18,12 @@ feature "As a user I want to sort shops for various criteria" do
 
     visit root_path
     click_link 'Power Outlets'
-    save_test
     page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
   scenario "can sort by noise" do
   skip 'waiting for adam to fix the pundit stuff'
   visit root_path
     click_link 'Noise'
-    save_test
     page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
 
