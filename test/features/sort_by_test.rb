0423b5c2b861783762032fc4d6c14da1e143da78
Adam Hendricksen <adam@nird.us>
2014-06-02 20:01:33 -0700

    Tests are ready

--- test/features/sort_by_test.rb
+++ test/features/sort_by_test.rb
@@ -2,27 +2,22 @@ require "test_helper"
 
 feature "As a user I want to sort shops for various criteria" do
   scenario "can sort by wifi_down" do
-    skip 'waiting for adam to fix the pundit stuff'
     visit root_path
     click_link 'Download Speed'
     page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
   scenario "can sort by wifi_up" do
-    skip 'waiting for adam to fix the pundit stuff'
     visit root_path
     click_link 'Upload Speed'
     page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
   scenario "can sort by power" do
-  skip 'waiting for adam to fix the pundit stuff'
-
     visit root_path
     click_link 'Power Outlets'
     page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
   scenario "can sort by noise" do
-  skip 'waiting for adam to fix the pundit stuff'
-  visit root_path
+    visit root_path
     click_link 'Noise'
     page.body.index('Richmond Beach Coffee Company') <  page.body.index('Terabtye')
   end
