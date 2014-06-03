f0f8c0abe68ff92310e2a0cedf0cd4683e9376ec
Adam Hendricksen <adam@nird.us>
2014-06-02 19:48:17 -0700

    Building shop authorization and making tests work

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,34 +1,50 @@
 class ShopsController < ApplicationController
-  before_action :set_shop, only: [:edit, :show, :update, :destroy]
-  before_action :authenticate_user!, except: [:show, :index, :sort_wifi_up]
+  before_action :set_shop, only: [:show]
+  before_action :authorize_new_shop, only: [:new]
+  before_action :authorize_created_shop, only: [:create]
+  before_action :set_and_authorize_shops, only: [ :index,
+                                                  :sort_wifi_up,
+                                                  :sort_wifi_down,
+                                                  :sort_power,
+                                                  :sort_noise]
+  before_action :set_and_authorize_shop, only:  [ :edit,
+                                                  :update,
+                                                  :destroy]
+  after_action :verify_authorized, only: [:index,
+                                          :sort_wifi_up,
+                                          :sort_wifi_down,
+                                          :sort_power,
+                                          :sort_noise]
+  after_action :verify_authorized, only: [:new,
+                                          :create,
+                                          :update,
+                                          :destroy]
+
 
   def index
-    @shops = Shop.all
   end
 
   def sort_wifi_up
-    @shops = Shop.all.reorder('wifi_up').reverse_order
+    @shops = @shops.reorder('wifi_up').reverse_order
     render "index"
   end
   def sort_wifi_down
-    @shops = Shop.all.reorder('wifi_down').reverse_order
+    @shops = @shops.reorder('wifi_down').reverse_order
     render "index"
   end
   def sort_noise
-    @shops = Shop.all.reorder('noise').reverse_order
+    @shops = @shops.reorder('noise').reverse_order
     render "index"
   end
   def sort_power
-    @shops = Shop.all.reorder('outlet_rating').reverse_order
+    @shops = @shops.reorder('outlet_rating').reverse_order
     render "index"
   end
 
   def new
-    @shop = Shop.new
   end
 
   def create
-    @shop = Shop.new shop_params
     if @shop.save!
       flash[:success] = "data saved in the datebase bro"
       redirect_to root_path
@@ -60,8 +76,33 @@ class ShopsController < ApplicationController
 private
 
   def set_shop
-    @shop = Shop.find(params[:id])
+    if params[:id]
+      @shop = Shop.find(params[:id])
+    else
+      Shop.new shop_params
+    end
   end
+
+  def set_and_authorize_shops
+    @shops = Shop.all
+    authorize @shops
+  end
+
+  def authorize_new_shop
+    @shop = Shop.new
+    authorize @shop
+  end
+
+  def authorize_created_shop
+    @shop = Shop.new shop_params
+    authorize @shop
+  end
+
+  def set_and_authorize_shop
+    set_shop
+    authorize @shop
+  end
+
   def shop_params
     params.require(:shop).permit  :name,
                                   :address,
