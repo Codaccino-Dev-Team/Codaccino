4786f2b0872ce2def274d0ae5c63909e3ac9b444
David Baynes <dlbaynes@gmail.com>
2014-06-02 22:09:13 -0700

    Fix validation error

--- app/models/dynamic_rating.rb
+++ app/models/dynamic_rating.rb
@@ -4,8 +4,6 @@ class DynamicRating < ActiveRecord::Base
   
   belongs_to :user
   
-  validates_inclusion_of :noise, :in=>[0,1,2,3,4,5], :message =>"Please enter a value from 0 to 5 for Noise rating"
-  
-  
+
 end
 
